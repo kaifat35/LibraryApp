bea5cdfebee4a7562ae3dc7a78effcae662a7720
Алексей <mr-kaifat@yandex.ru>
2025-05-21 19:36:49 +0300

    добавлен  BooksDB , Repository

---
+++ app/src/main/java/com/example/libraryapp/repository/Repository.kt
@@ -0,0 +1,10 @@
+package com.example.libraryapp.repository
+
+import com.example.libraryapp.room.BookEntity
+import com.example.libraryapp.room.BooksDB
+
+class Repository(val booksDB: BooksDB) {
+    suspend fun addBookToRoom(bookEntity: BookEntity){
+        booksDB.bookDao().addBook(bookEntity)
+    }
+}
